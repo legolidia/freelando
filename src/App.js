8d92cdeb6a2bead9614c4e0b5122cf282ff1d54a
legolidia <lidiacarolinaandrade@gmail.com>
2023-05-31 17:50:45 -0300

    Primeiro módulo concluído.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Card } from "./components/Card/Card";
 import { Estilos } from "./components/EstilosGlobais/Estilos";
 
 function App() {
   return (
     <div>
       <Estilos/>
+      <Card>
       <h1> Freelando </h1>
+      </Card>
     </div>
   );
 }
