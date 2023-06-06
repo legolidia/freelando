17261a5444f534faa014fe3f6da609bdef02cf78
legolidia <lidiacarolinaandrade@gmail.com>
2023-06-05 21:42:46 -0300

    estilos de fontes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Card } from "./components/Card/Card";
 import { Estilos } from "./components/EstilosGlobais/Estilos";
 import { ProvedorTema } from "./components/ProvedorTema/ProvedorTema";
+import { Tipografia } from "./components/Tipografia/Tipografia";
+
 
 function App() {
   return (
     <ProvedorTema>
       <Estilos />
-      <Card>
-      <h1> Freelando </h1>
-      </Card>
+        <Card>
+
+          <Tipografia variante="h1" componente="h1"> 
+            Freelando  
+          </Tipografia>
+
+          <Tipografia variante="body1" componente="body1">
+            Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. 
+            Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho. 
+          </Tipografia>
+          
+        </Card>
     </ProvedorTema>
   );
 }
